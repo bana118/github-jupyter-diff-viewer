09fe7d44e08ef4fdf0f4f0df43f9754eb27d4009
bana <bana.titech@gmail.com>
2020-07-18 11:46:50 +0900

    get jupyter file from pull request

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 const target = document.body;
 const observer = new MutationObserver(records => {
-	const pattern = /https:\/\/github.com\/(.+)\/(.+)\/pull\/(\d+)\/files/
+	const pattern = /https:\/\/github.com\/(.+)\/(.+)\/pull\/(\d+)\/files/;
 	if (pattern.test(location.href)) {
 		const prData = location.href.match(pattern);
 		const owner = prData[1];
@@ -9,17 +9,38 @@ const observer = new MutationObserver(records => {
 		chrome.storage.local.get(null, function (items) {
 			const token = items.githubAccessToken;
 			const url = `https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/files`;
-			var request = new XMLHttpRequest();
-			request.open('GET', url);
+			const request = new XMLHttpRequest();
+			request.open("GET", url);
 			request.setRequestHeader("Authorization", `token ${token}`);
+			request.setRequestHeader("Accept", "application/vnd.github.v3.raw");
 			request.onreadystatechange = function () {
 				if (request.readyState != 4) {
-					console.log("now sending...")
+					console.log("now pr info requeset sending...")
 				} else if (request.status != 200) {
 					console.error(`Github API fail! status=${request.status}`)
 				} else {
-					const result = request.responseText;
-					console.log(result);
+					const prFilesInfo = request.responseText;
+					const prFilesInfoJson = JSON.parse(prFilesInfo);
+					const jupyterFileRegExp = /\.ipynb$/;
+					const jupyterFilesInfoJson = prFilesInfoJson.filter(prFileInfoJson => jupyterFileRegExp.test(prFileInfoJson.filename));
+					for(jupyterFileInfoJson of jupyterFilesInfoJson){
+						const blobUrl = `https://api.github.com/repos/${owner}/${repo}/git/blobs/${jupyterFileInfoJson.sha}`;
+						console.log(blobUrl);
+						const rawFileRequest = new XMLHttpRequest();
+						rawFileRequest.open("GET", blobUrl);
+						rawFileRequest.setRequestHeader("Authorization", `token ${token}`);
+						rawFileRequest.setRequestHeader("Accept", "application/vnd.github.v3.raw");
+						rawFileRequest.onreadystatechange = function () {
+							if(rawFileRequest.readyState !== 4){
+								console.log("now raw file info requeset sending...")
+							}else if(rawFileRequest.status != 200){
+								console.error(`Github API fail! status=${request.status}`)
+							}else{
+								console.log(rawFileRequest.responseText);
+							}
+						}
+						rawFileRequest.send(null);
+					}
 				}
 			};
 			request.send(null);
