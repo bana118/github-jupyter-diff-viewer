aca259189d58ebaa1f3f1d0b49528ff4ae149cf4
bana118 <bana.titech@gmail.com>
2020-08-31 18:55:09 +0900

    remove comment

--- js/content.js
+++ js/content.js
@@ -143,7 +143,6 @@ const observer = new MutationObserver(records => {
 			request.setRequestHeader("Accept", "application/vnd.github.v3.raw");
 			request.onreadystatechange = function () {
 				if (request.readyState != 4) {
-					// console.log("now pr info requeset sending...");
 					const existPrNowLoadingElement = document.getElementById(`${prefix}-now-loading-pr-${commitHash}`);
 					if (existPrNowLoadingElement == null) {
 						const prNowLoadingElement = document.createElement("div");
@@ -179,7 +178,6 @@ const observer = new MutationObserver(records => {
 						rawFileRequest.setRequestHeader("Accept", "application/vnd.github.v3.raw");
 						rawFileRequest.onreadystatechange = function () {
 							if (rawFileRequest.readyState !== 4) {
-								// console.log("now raw file info requeset sending...");
 								const existFileNowLoadingElement = document.getElementById(`${prefix}-now-loading-file-${diffHash}`);
 								if (existFileNowLoadingElement == null) {
 									const fileNowLoadingElement = document.createElement("div");
